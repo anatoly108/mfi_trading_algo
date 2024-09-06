ba960d2f4cf530c3f9eb5db073984bd31e3ad557
Anatoly Vasilyev <anatoly.developer@gmail.com>
2024-09-06 16:58:21 +0200

    fix

--- mfi_infinite_trade.py
+++ mfi_infinite_trade.py
@@ -13,7 +13,7 @@ import concurrent.futures
 from mfi_functions import setup_logging, calculate_mfi, \
                             find_extrema, plot_asset, get_candles, MFI_TIMEINTERVAL, \
                             run_mfi_trading_algo, usd_to_quantity
-from mfi_analysis import main
+from mfi_analysis import mfi_analysis_main
 
 def run_mfi_trading_algo_wrapper(**kwargs):
     # wrapper to apply different logging in this subprocess/thread
